722a72f59cb39471df8c28bead3a39b3fb4934ca
周志钦 <zhiqin.zhou@ucarinc.com>
2021-06-24 18:30:57 +0800

    2021-6-24:更换数据库为远程数据库

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 支付
@@ -78,6 +79,18 @@ public class PaymentController {
     @GetMapping(value = "/lb")
     public String getPaymentLB() {
         return serverPort;//返回服务接口
+    }
+
+    @GetMapping(value = "/feign/timeout")
+    public String paymentFeignTimeout()
+    {
+        // 业务逻辑处理正确，但是需要耗费3秒钟
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return serverPort;
     }
 
 
